0c6af9c977250c364a22e33cb418eba436ed49a7
Riyaadaws <riyaadryklief92@gmail.com>
2021-04-17 17:38:39 +0200

    First upload

---
+++ app/src/androidTest/java/com/example/riyaaddevspace/AboutTest.java
@@ -0,0 +1,12 @@
+package com.example.riyaaddevspace;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AboutTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
---
+++ app/src/androidTest/java/com/example/riyaaddevspace/CalculateTest.java
@@ -0,0 +1,12 @@
+package com.example.riyaaddevspace;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
